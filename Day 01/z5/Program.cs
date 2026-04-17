21d4e658846a0b8ab6d95753a07cc80d973e88b8
totawi <sharipovamila83@gmail.com>
2026-04-17 11:21:07 +0300

    e

--- Day 01/z5/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Write("Введите трехзначное число: ");
-int n = int.Parse(Console.ReadLine());
-
-int first = n / 100;
-int second = (n / 10) % 10;
-
-int maxDigit = Math.Max(first, second);
-
-Console.WriteLine($"Из цифр {first} и {second} самая большая: {maxDigit}");
--- Day 01/z8/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Write("Введите N: ");
-int n = int.Parse(Console.ReadLine());
-double sumFor = 0;
-
-for (int i = 1; i <= n; i++)
-{
-    sumFor += 1.0 / i; 
-}
-Console.WriteLine("Цикл for: " + sumFor.ToString("F4"));

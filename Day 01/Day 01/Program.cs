d522b09cf4a9d020deadb638578d46ff6789417a
totawi <sharipovamila83@gmail.com>
2026-04-17 11:18:52 +0300

    Ready

---
+++ Day 01/Day 01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+int mon = 450;
+int sys = 3000;
+int keyboard = 200;
+int mouse = 100;
+Console.WriteLine($"Цена системного блока: {sys}, цена монитора: {mon}, цена клавиатуры: {keyboard}, ыена мышки: {mouse}");
+
+Console.WriteLine("Сколько копьютеров вы хотите приобрести? ");
+int value = int.Parse(Console.ReadLine());
+
+int total = (mon + sys + keyboard + mouse) * value;
+Console.WriteLine($"{value} компьютеров обойдуться вам в {total}");
---
+++ Day 01/z4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите x: ");
+double x = double.Parse(Console.ReadLine());
+double y;
+
+if (x > 2)
+{
+    y = x * Math.Pow(x - 2, 1.0 / 3.0);
+}
+else if (x == 2)
+{
+    y = x * Math.Sin(2 * x);
+}
+else 
+{
+    y = Math.Exp(-2 * x) * Math.Cos(2 * x);
+}
+
+Console.WriteLine($"Результат y = {y}");
